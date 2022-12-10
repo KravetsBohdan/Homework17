9c66e4af5697fcc0616ac627e1c3bf63367bac01
KravetsBohdan <bodyakravets@gmail.com>
2022-12-10 08:29:56 +0200

    Add feature to work with files

---
+++ src/main/java/com/bkravets/utils/FileUtils.java
@@ -0,0 +1,43 @@
+package com.bkravets.utils;
+
+import com.bkravets.models.Person;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class FileUtils {
+
+    public static void downloadPicture(String url, String fileName) {
+        Path path = Path.of(fileName);
+
+        try (InputStream inputStream = new URL(url).openStream()) {
+            Files.copy(inputStream, path);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    public static void writePeopleToCsv(List<Person> people) {
+        List<String> csvRepresentation = people.stream()
+                .map(person -> String.format("%s,%d,%s", person.name(), person.age(), person.sex()))
+                .toList();
+
+        Path out = Path.of("out", "result.csv");
+
+
+        try {
+            Files.createDirectory(Path.of("out"));
+            Files.write(out, csvRepresentation);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
